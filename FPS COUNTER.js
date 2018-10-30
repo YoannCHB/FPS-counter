0dce84b2c793edd7415f8f3c3da44d5a3a5be7d6
Yoann Charbonnier <39668280+YoannCHB@users.noreply.github.com>
2018-10-30 15:22:45 +0100

    Update FPS COUNTER.js

--- FPS COUNTER.js
+++ FPS COUNTER.js
@@ -38,9 +38,33 @@ function appendDiv(text){
 		c.innerHTML = "<p style='font-size: 16px; color: #00FF00; margin: 10px'>FPS: "+text+"</p>";
 	}
 }
+
+var stats = {
+    speedCo: null
+}
+function speedConnexion(){
+    let size = 32413;
+    let time1 = new Date();
+    time1 = time1.getTime();
+    let img = new Image();
+    img.src = "https://ak.picdn.net/assets/cms/14c40a5a7a7f50788cf9e346071ab49ad1201b12-17d705f0349ed08f5387b39c7644054a59affe52-LOHP_vector_module_shutterstock_307324316-min.jpg?"+time1;
+    img.onload = function(){
+        let time2 = new Date();
+        time2 = time2.getTime();
+        let  ms = time2-time1;
+        let final = Math.round(size/ms*100)/100;
+        stats.speedCo = final;
+    }
+}
+
 /*
+setTimeout(function(){
+    console.log(stats.speedCo+" ko/s");
+}, 1500);
+
 setInterval(function(){
 	appendDiv(fpsCounter.fps);
 }, 1500);
 */
 FPSloop();
+speedConnexion();
